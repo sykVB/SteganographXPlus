e1494bae9849eadbca9f42362ee38a9c9836817a
sykVB <sykbhowal@gmail.com>
2025-05-10 23:48:15 +0530

    Add files via upload

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(39, 33);
+            this.txtFilePath.Margin = new System.Windows.Forms.Padding(4);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(449, 22);
+            this.txtFilePath.TabIndex = 0;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(512, 30);
+            this.btnBrowse.Margin = new System.Windows.Forms.Padding(4);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(100, 28);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(248, 80);
+            this.btnDecrypt.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(100, 28);
+            this.btnDecrypt.TabIndex = 2;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 150);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(119, 150);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.ReadOnly = true;
+            this.txtPassword.Size = new System.Drawing.Size(369, 22);
+            this.txtPassword.TabIndex = 4;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(119, 206);
+            this.txtMessage.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.ReadOnly = true;
+            this.txtMessage.Size = new System.Drawing.Size(369, 278);
+            this.txtMessage.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 206);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Message";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 500);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtFilePath);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        byte[] fileBytes = null;
+        int width = 0;
+        int height = 0;
+        int bmpType = 0;
+        int headerSize = 0;
+        string signature = "Encrypt@LeeLuSoft.BlogSpot.com";
+        uint constant = 0x8088405;
+        bool isFound = false;
+        List<byte> pixels;
+        uint password = 0;
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtPassword.Text = string.Empty;
+                txtMessage.Text = string.Empty;
+
+                txtFilePath.Text = openFileDialog1.FileName;
+                fileBytes = File.ReadAllBytes(openFileDialog1.FileName);
+
+                width = (int)(fileBytes[21] * Math.Pow(16, 8) + fileBytes[20] * Math.Pow(16, 4) + fileBytes[19] * Math.Pow(16, 2) + fileBytes[18]);
+                height = (int)(fileBytes[25] * Math.Pow(16, 8) + fileBytes[24] * Math.Pow(16, 4) + fileBytes[23] * Math.Pow(16, 2) + fileBytes[22]);
+                bmpType = (int)fileBytes[28];
+                headerSize = (int)fileBytes[10];
+
+                if (width * height >= 488)
+                {
+
+
+                    isFound = false;
+
+                    if (bmpType == 24)
+                    {
+                        btnDecrypt.Enabled = true;
+                        pixels = new List<byte>();
+                        int size = 3 * width * height;
+                        int zeroPad = width % 4;
+
+                        for (int i = 0; i < height; i++)
+                            for (int j = 0; j < (width * 3); j++)
+                                pixels.Add(fileBytes[(headerSize + ((size + zeroPad * height)) - (i + 1) * (3 * width + zeroPad)) + j]);
+                    }
+                    else if (bmpType == 16)
+                    {
+                        btnDecrypt.Enabled = true;
+                        pixels = new List<byte>();
+                        int size = 2 * width * height;
+
+                        for (int i = 0; i < height; i++)
+                            for (int j = 0; j < (width * 2); j++)
+                                pixels.Add(fileBytes[(headerSize + size - (i + 1) * (2 * width)) + j]);
+
+                    }
+                    else if (bmpType == 32)
+                    {
+                        btnDecrypt.Enabled = true;
+                        pixels = new List<byte>();
+                        int size = 4 * width * height;
+
+                        for (int i = 0; i < height; i++)
+                            for (int j = 0; j < (width * 4); j++)
+                                pixels.Add(fileBytes[(headerSize + size + -(i + 1) * (4 * width)) + j]);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Not Supported by Steganography X Plus");
+                        btnDecrypt.Enabled = false;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Not Supported by Steganography X Plus");
+                    btnDecrypt.Enabled = false;
+                }
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            Stopwatch sw;
+            sw = Stopwatch.StartNew();
+            txtPassword.Text = string.Empty;
+            txtMessage.Text = string.Empty;
+            int zeroPad = 0;
+            int jump = 0;
+
+            if (bmpType == 24)
+            {
+                jump = 3;
+                zeroPad = width % 4;
+            }
+            else if (bmpType == 32)
+                jump = 4;
+            else if (bmpType == 16)
+                jump = 2;
+
+            for (uint i = 1; i <= 0xFFFFFFFF; i++)
+            {
+                uint multiplier = i;
+                int a = 0;
+                byte b = 0;
+
+
+                for (int j = jump - 1, k = 0, m = -1; j < 240 * jump; j += jump, k++)
+                {
+
+                    for (int n = 0; n < jump - 1; n++)
+                    {
+                        multiplier = multiplier * constant;
+                        multiplier++;
+                    }
+
+                    multiplier = multiplier * constant;
+                    byte val = (byte)((multiplier & 0xFF000000) >> 24);
+                    multiplier++;
+
+                    if (zeroPad > 0 && (j + 1) % (width * 3) == 0)
+                    {
+                        for (int n = 0; n < zeroPad; n++)
+                        {
+                            multiplier = multiplier * constant;
+                            multiplier++;
+                        }
+                    }
+
+                    if (k == 0)
+                    {
+                        a = 0;
+                        b = 0;
+                        m++;
+                    }
+
+                    a = (a + (((pixels[j] ^ val) & 1) << k));
+                    b = (byte)((byte)(signature[m] << (7 - k)) >> (7 - k));
+
+                    if (a != b)
+                        break;
+
+                    if (k == 7)
+                        k = -1;
+
+                    if (j == 240 * jump - 1)
+                        isFound = true;
+                }
+
+                if (isFound)
+                {
+                    txtPassword.Text = Convert.ToString(i);
+                    password = i;
+                    break;
+                }
+
+            }
+
+
+            if (isFound)
+            {
+                isFound = false;
+
+                uint multiplier = password;
+                int a = 0;
+                List<byte> message = new List<byte>();
+
+                for (int j = jump - 1, k = 0, m = -1; j < pixels.Count; j += jump, k++)
+                {
+
+                    for (int n = 0; n < jump - 1; n++)
+                    {
+                        multiplier = multiplier * constant;
+                        multiplier++;
+                    }
+                    multiplier = multiplier * constant;
+                    byte val = (byte)((multiplier & 0xFF000000) >> 24);
+                    multiplier++;
+
+                    if (zeroPad > 0 && (j + 1) % (width * 3) == 0)
+                    {
+                        for (int n = 0; n < zeroPad; n++)
+                        {
+                            multiplier = multiplier * constant;
+                            multiplier++;
+                        }
+                    }
+
+                    if (k == 0)
+                    {
+                        a = 0;
+                        m++;
+                    }
+
+                    a = (a + (((pixels[j] ^ val) & 1) << k));
+
+                    if (k == 7)
+                    {
+                        message.Add((byte)a);
+                        if (a == 0)
+                            break;
+                        k = -1;
+                    }
+                }
+
+                message = message.GetRange(60, message.Count - 61);
+                txtMessage.Text = Encoding.UTF8.GetString(message.ToArray());
+
+                sw.Stop();
+            }
+            else
+            {
+                sw.Stop();
+                MessageBox.Show("Not found");
+
+            }
+
+            MessageBox.Show("Execution time:" + Convert.ToString(sw.ElapsedMilliseconds) + "ms");
+        }
+
+    }
+}
